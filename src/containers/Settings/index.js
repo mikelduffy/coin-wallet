30f707ad1da7beef04cf1c09dc2d829262ddd6f3
Mikel Duffy <mikelduffy1@gmail.com>
2018-03-12 00:48:51 +0800

    Settings style fixes

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -4,6 +4,7 @@ import Dialog from 'material-ui/Dialog'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
 import Checkbox from 'material-ui/Checkbox'
+import Divider from 'material-ui-next/Divider'
 
 class Settings extends Component {
   static propTypes = {
@@ -73,91 +74,116 @@ class Settings extends Component {
 
   render() {
     return (
-      <div>
-        <Dialog
-          title="Settings"
-          modal={false}
-          open={this.props.open}
-          onRequestClose={this.handleClose}
-          autoScrollBodyContent={true}
-        >
-          <h4>Change Password</h4>
-          <TextField
-            id="oldPassword"
-            floatingLabelText="Old Password"
-            type="password"
-            value={this.state.oldPasswordValue}
-            errorText={this.state.oldPasswordError}
-            onChange={this.handleInputChange}
-          />
-          <br />
-          <TextField
-            id="newPassword"
-            floatingLabelText="New Password"
-            type="password"
-            value={this.state.newPasswordValue}
-            errorText={this.state.newPasswordError}
-            onChange={this.handleInputChange}
-          />
-          <br />
-          <TextField
-            id="confirmNewPassword"
-            floatingLabelText="Confirm New Password"
-            type="password"
-            value={this.state.confirmNewPasswordValue}
-            errorText={this.state.confirmNewPasswordError}
-            onChange={this.handleInputChange}
-          />
-          <br />
-          <RaisedButton
-            label="Submit"
-            primary
-            disabled={
-              !this.state.oldPasswordValue ||
-              !this.state.newPasswordValue ||
-              !this.state.confirmNewPasswordValue ||
-              this.state.newPasswordValue !== this.state.confirmNewPasswordValue
-            }
-            onClick={this.handlePasswordChange}
-          />
-          <h4>Backup Wallet</h4>
-          <TextField
-            id="backupPassword"
-            floatingLabelText="Password"
-            type="password"
-            value={this.state.backupPasswordValue}
-            errorText={this.state.backupPasswordError}
-            onChange={this.handleInputChange}
-          />
-          <br />
-          <RaisedButton
-            label="Download"
-            primary
-            disabled={!this.state.backupPasswordValue}
-            onClick={this.handleDownloadBackup}
-          />
-          <h4>Complete Reset</h4>
-          <Checkbox
-            label="I understand that this will delete my wallet, and that there are no recovery options if I have not already downloaded a backup."
-            checked={this.state.confirmReset}
-            onCheck={() => {
-              this.setState({ confirmReset: !this.state.confirmReset })
-            }}
-            style={{
-              marginBottom: '16px',
-            }}
-          />
-          <RaisedButton
-            label="Reset"
-            secondary
-            disabled={!this.state.confirmReset}
-            onClick={() => {
-              this.props.handleReset()
-              this.handleClose()
-            }}
-          />
-        </Dialog>
-      </div>
+      <Dialog
+        title="Settings"
+        modal={false}
+        open={this.props.open}
+        onRequestClose={this.handleClose}
+        autoScrollBodyContent={true}
+      >
+        <Divider
+          style={{
+            margin: '0px -24px 0px -24px',
+          }}
+        />
+        <h4>Change Password</h4>
+        <TextField
+          id="oldPassword"
+          floatingLabelText="Old Password"
+          type="password"
+          fullWidth
+          value={this.state.oldPasswordValue}
+          errorText={this.state.oldPasswordError}
+          onChange={this.handleInputChange}
+          style={{
+            marginTop: -15,
+          }}
+        />
+        <TextField
+          id="newPassword"
+          floatingLabelText="New Password"
+          type="password"
+          fullWidth
+          value={this.state.newPasswordValue}
+          errorText={this.state.newPasswordError}
+          onChange={this.handleInputChange}
+          style={{
+            marginTop: -15,
+          }}
+        />
+        <TextField
+          id="confirmNewPassword"
+          floatingLabelText="Confirm New Password"
+          type="password"
+          fullWidth
+          value={this.state.confirmNewPasswordValue}
+          errorText={this.state.confirmNewPasswordError}
+          onChange={this.handleInputChange}
+          style={{
+            marginTop: -15,
+          }}
+        />
+        <RaisedButton
+          label="Submit"
+          primary
+          disabled={
+            !this.state.oldPasswordValue ||
+            !this.state.newPasswordValue ||
+            !this.state.confirmNewPasswordValue ||
+            this.state.newPasswordValue !== this.state.confirmNewPasswordValue
+          }
+          onClick={this.handlePasswordChange}
+        />
+        <Divider
+          style={{
+            margin: '20px -24px 0px -24px',
+          }}
+        />
+        <h4>Backup Wallet</h4>
+        <TextField
+          id="backupPassword"
+          floatingLabelText="Password"
+          type="password"
+          fullWidth
+          value={this.state.backupPasswordValue}
+          errorText={this.state.backupPasswordError}
+          onChange={this.handleInputChange}
+          style={{
+            marginTop: -15,
+          }}
+        />
+        <RaisedButton
+          label="Download"
+          primary
+          disabled={!this.state.backupPasswordValue}
+          onClick={this.handleDownloadBackup}
+        />
+        <Divider
+          style={{
+            margin: '20px -24px 0px -24px',
+          }}
+        />
+        <h4>Complete Reset</h4>
+        <Checkbox
+          label="I understand that this will delete my wallet, and that there are no recovery options if I have not already downloaded a backup."
+          checked={this.state.confirmReset}
+          onCheck={() => {
+            this.setState({ confirmReset: !this.state.confirmReset })
+          }}
+          style={{
+            marginBottom: '16px',
+          }}
+        />
+        <RaisedButton
+          label="Reset"
+          secondary
+          disabled={!this.state.confirmReset}
+          onClick={() => {
+            this.props.handleReset()
+            this.handleClose()
+          }}
+        />
+      </Dialog>
     )
   }
 }
