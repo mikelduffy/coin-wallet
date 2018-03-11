09f36445a1657036851dd0e3043beff86711434a
Mikel Duffy <mikelduffy1@gmail.com>
2018-03-11 15:49:09 +0800

    Refactors navigation to use material ui 1.0 beta

--- src/containers/Navigation/index.js
+++ src/containers/Navigation/index.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import AppBar from 'material-ui/AppBar'
-import IconButton from 'material-ui/IconButton'
-import SettingsIcon from 'material-ui/svg-icons/action/settings'
+import AppBar from 'material-ui-next/AppBar'
+import Toolbar from 'material-ui-next/Toolbar'
+import Typography from 'material-ui-next/Typography'
+import IconButton from 'material-ui-next/IconButton'
+import SettingsIcon from 'material-ui-icons/Settings'
+import { withStyles } from 'material-ui-next/styles'
 
-const Navigation = ({ handleToggleSettings }) => (
-  <nav>
-    <AppBar
-      title="Coin Wallet"
-      showMenuIconButton={false}
-      iconElementRight={
-        <IconButton onClick={handleToggleSettings}>
-          <SettingsIcon />
-        </IconButton>
-      }
-    />
-  </nav>
+const styles = theme => ({
+  title: {
+    flex: 1,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+})
+
+const Navigation = ({ handleToggleSettings, classes, coin }) => (
+  <AppBar position="absolute">
+    <Toolbar>
+      <Typography
+        variant="title"
+        color="inherit"
+        noWrap
+        className={classes.title}
+      >
+        {coin.toUpperCase()} Wallet
+      </Typography>
+      <IconButton className={classes.button} onClick={handleToggleSettings}>
+        <SettingsIcon color="white" />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
 )
 
 Navigation.propTypes = {
+  coin: PropTypes.string.isRequired,
   handleToggleSettings: PropTypes.func.isRequired,
 }
 
-export default Navigation
+export default withStyles(styles)(Navigation)
